5531f1914f41b60de691f9e27fa33dfb4b8d0a42
Nakajima2nd <gp38ate@gmail.com>
2024-10-26 18:14:20 +0900

    自動デプロイを再修正

--- app/api/deploy/route.ts
+++ app/api/deploy/route.ts
@@ -1,4 +1,7 @@
+import { NextResponse } from "next/server"
+
 export const GET = async () => {
     const url = `${process.env.DEPLOY_HOOK_URL}`
     fetch(url)
+    return NextResponse.json({ ok: true })
 }
